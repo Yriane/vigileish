ff6d20133bbbada153288fa974450cf558c02bee
Yriane <86128931+Yriane@users.noreply.github.com>
2025-07-07 18:07:36 -0300

    Update firebase-config.js

--- VigiLeish/js/firebase-config.js
+++ VigiLeish/js/firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA_ZK5i9aPppkp7G93dTJ1mvQMi7ub1O7I",
-  authDomain: "vigialeish.firebaseapp.com",
-  projectId: "vigialeish",
-  storageBucket: "vigialeish.firebasestorage.app",
-  messagingSenderId: "575123199001",
-  appId: "1:575123199001:web:c9b08d12e719fe0b9c4688",
-  measurementId: "G-70VC02N00Q"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
 
 firebase.initializeApp(firebaseConfig);

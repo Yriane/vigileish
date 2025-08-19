85010ef74eefd90185ed942c52e05c31920ad38d
Yriane <86128931+Yriane@users.noreply.github.com>
2025-08-19 06:49:05 -0300

    Update firebase-config.js

--- VigiLeish/js/firebase-config.js
+++ VigiLeish/js/firebase-config.js
@@ -1,12 +1,13 @@
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
-  measurementId: "..."
+  apiKey: "AIzaSyA_ZK5i9aPppkp7G93dTJ1mvQMi7ub1O7I",
+  authDomain: "vigialeish.firebaseapp.com",
+  projectId: "vigialeish",
+  storageBucket: "vigialeish.firebasestorage.app",
+  messagingSenderId: "575123199001",
+  appId: "1:575123199001:web:c9b08d12e719fe0b9c4688",
+  measurementId: "G-70VC02N00Q"
 };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
